e04f8259e5318b7e285e7a8933272a335b6cb308
javaashdod <java.ashdod@gmail.com>
2016-07-31 14:34:38 +0300

    inserting data

--- app/src/main/java/tomer/edu/dbdemos/MainActivity.java
+++ app/src/main/java/tomer/edu/dbdemos/MainActivity.java
@@ -1,7 +1,9 @@
 package tomer.edu.dbdemos;
 
 import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +11,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle;
+    EditText etDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,21 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        TodosDBHelper helper = new TodosDBHelper(this);
-        ContentValues values = new ContentValues();
-        values.put("title", "Hello");
-        values.put("description", "World");
-        helper.getWritableDatabase().insert("Todos", null, values);
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etDescription = (EditText) findViewById(R.id.etDescription);
     }
 
     @Override
@@ -55,5 +48,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void save(View view) {
+        //1) init the helper:
+        TodosDBHelper helper = new TodosDBHelper(this);
+
+        //2) run your query:
+        SQLiteDatabase db = helper.getWritableDatabase();
+        //db.execSQL("INSERT INTO Todos VALUES('" + getetTitle() + "', '" + getDescription() + "')");
+
+        ContentValues values = new ContentValues();
+        values.put("Title", getetTitle());
+        values.put("Description", getDescription());
+
+        db.insert("Todos", null, values);
+
+    }
+
+    String getetTitle() {
+        return etTitle.getText().toString();
+    }
+
+    public String getDescription() {
+        return etDescription.getText().toString();
     }
 }
